ad8acac95a6279eb9002ecbf90c835ecb33afa59
pinaalu <pinaalu@uade.edu.ar>
2024-06-14 14:53:03 -0300

    primera implementacion de clases

--- src/src/Vehiculo.java
+++ src/src/Vehiculo.java
@@ -1,2 +1,12 @@
 public class Vehiculo {
+
+    private int precio;
+
+    public Vehiculo(int precio){
+        this.precio = precio;
+    }
+
+    public int getPrecio(){
+        return this.precio;
+    }
 }
