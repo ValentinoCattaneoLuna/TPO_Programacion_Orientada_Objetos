3375ada8acfea093d334c24667a9e914c3d54f0d
valentinocattaneoluna <valencattaneo04@gmail.com>
2024-06-14 16:15:57 -0300

    agregamos toString y cambiamos la variable de fecha

--- src/Pedido.java
+++ src/Pedido.java
@@ -1,25 +1,32 @@
-public class Pedido {
 
+import java.util.Date;
+public class Pedido {
     private int precioFinal;
     private Servicio serv;
     private Cliente cliente;
     private Estado estado;
-    private java.util.Date fecha;
+    private Date fecha;
 
 
     public Pedido(Servicio serv, Cliente cliente){
         this.serv = serv;
         this.cliente = cliente;
-        this.estado = Estado.enProceso;
+        this.estado = Estado.EN_PROCESO;
+        this.fecha = new Date();
     }
 
     public  void terminarPedido(){
-        this.estado = Estado.Terminado;
+        this.estado = Estado.TERMINADO;
     }
     public void calcularPrecio(){
          this.precioFinal = this.cliente.getVehiculo().getPrecio() + this.serv.getPrecio();
     }
     public int getPrecioFinal() {
         return precioFinal;
+    }
+
+    @Override
+    public String toString() {
+        return "Pedido{" + serv +" " + cliente +" " + cliente.getVehiculo() +" " +  estado + '}';
     }
 }
