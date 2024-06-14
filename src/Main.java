2734aa5c750a73b6d14fdfe60396322ae6058bd4
valentinocattaneoluna <valencattaneo04@gmail.com>
2024-06-14 16:15:17 -0300

    agregamos teste de la implementacion

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-
+        Sistema lavaderoPepe = new Sistema();
+        Servicio lavadoCompleto = new Servicio("Lavado completo",10000);
+        Servicio lavadoExpress = new Servicio("Lavado express",4000);
+        Vehiculo tornado = new Vehiculo("tornado",2000);
+        Vehiculo cuatrinashe = new Vehiculo("cuatrinashe",2500);
+        Vehiculo gol = new Vehiculo("gol",3500);
+        Cliente valentino = new Cliente("Valentino", gol,"1128384628");
+        Cliente mateo = new Cliente("Mateo", tornado, "2254123456");
+        Pedido pedido1 = new Pedido(lavadoCompleto, mateo);
+        Pedido pedido2 = new Pedido(lavadoExpress, valentino);
+        lavaderoPepe.agregarPedido(pedido1);
+        lavaderoPepe.agregarPedido(pedido2);
+        lavaderoPepe.terminarPedido(pedido2);
+        lavaderoPepe.mostrarTodo();
     }
 }
