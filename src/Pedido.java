101fa164d073c52b3f186fcfc7261b6b1060c0d2
valentinocattaneoluna <valencattaneo04@gmail.com>
2024-06-14 15:32:07 -0300

    agregamos constructor y terminarPedido

--- src/Pedido.java
+++ src/Pedido.java
@@ -7,6 +7,15 @@ public class Pedido {
     private java.util.Date fecha;
 
 
+    public Pedido(Servicio serv, Cliente cliente){
+        this.serv = serv;
+        this.cliente = cliente;
+        this.estado = Estado.enProceso;
+    }
+
+    public  void terminarPedido(){
+        this.estado = Estado.Terminado;
+    }
     public void calcularPrecio(){
          this.precioFinal = this.cliente.getVehiculo().getPrecio() + this.serv.getPrecio();
     }
