83a5dc0c45af8fc43604ed443ce8e5fbe3c27a0d
Valentino Cattaneo Luna <131176387+ValentinoCattaneoLuna@users.noreply.github.com>
2024-06-24 17:11:22 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,14 @@ public class Main {
         Vehiculo tornado = new Vehiculo("tornado",2000);
         Vehiculo cuatrinashe = new Vehiculo("cuatrinashe",2500);
         Vehiculo gol = new Vehiculo("gol",3500);
-        Cliente valentino = new Cliente("Valentino", gol,"1128384628");
-        Cliente mateo = new Cliente("Mateo", tornado, "2254123456");
+        Cliente valentino = new Cliente("Valentino", gol,"+5491128384628");
+        Cliente mateo = new Cliente("Mateo", tornado, "+5492267530090");
         Pedido pedido1 = new Pedido(lavadoCompleto, mateo);
         Pedido pedido2 = new Pedido(lavadoExpress, valentino);
         lavaderoPepe.agregarPedido(pedido1);
         lavaderoPepe.agregarPedido(pedido2);
+        lavaderoPepe.terminarPedido(pedido1);
         lavaderoPepe.terminarPedido(pedido2);
-
 
         List<Pedido> lavadosHoy = lavaderoPepe.obtenerTerminadosDelDia();
         lavaderoPepe.mostrarPedidos(lavadosHoy);
